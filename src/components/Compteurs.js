a1c94c27b7cde6b1391c036bce8afdc064296d4e
PierreM <7anUDQ2A3MVJkC7J@gmail.com>
2021-12-09 12:16:29 +0100

    button disappear

--- src/components/Compteurs.js
+++ src/components/Compteurs.js
@@ -2,12 +2,12 @@ import React, { useState } from "react";
 import "./Compteurs.css";
 
 const Compteurs = () => {
-  const [compteur, setCompteur] = useState(5);
-  console.log("HELLO");
+  const [compteur, setCompteur] = useState(1);
+  const [compteur2, setCompteur2] = useState(1);
 
   return (
     <div className="bgdCompteurs">
-      <span>COMPTEUR EXEMPLE </span>
+      <span>COMPTEUR 1 </span>
       <button
         onClick={() => {
           setCompteur(compteur + 1);
@@ -15,15 +15,36 @@ const Compteurs = () => {
       >
         +
       </button>
+
+      {compteur > 0 ? (
+        <button
+          onClick={() => {
+            setCompteur(compteur - 1);
+          }}
+        >
+          -
+        </button>
+      ) : null}
+
+      <span> {compteur} </span>
+      <span>COMPTEUR 2 </span>
       <button
         onClick={() => {
-          setCompteur(compteur - 1);
+          setCompteur2(compteur2 + 1);
+        }}
+      >
+        +
+      </button>
+      <button
+        onClick={() => {
+          setCompteur2(compteur2 - 1);
         }}
       >
         -
       </button>
-
-      <span> {compteur} </span>
+      <span> {compteur2} </span>
+      <span>TOTAL </span>
+      <span> {compteur + compteur2} </span>
     </div>
   );
 };
